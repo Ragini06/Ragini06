7c40fa6d45484bf8373de96f385d1898422e04f9
bitcoin <masanta.ragini@gmail.com>
2021-12-15 16:33:47 +0530

    added coinArray

--- src/component/Table.js
+++ src/component/Table.js
@@ -1,6 +1,8 @@
-import React, { useEffect, useSate, useRef } from "react";
-import { isEmpty } from 'lodash';
-import { useState } from "react/cjs/react.development";
+import React, { useEffect, useState, useRef } from "react";
+import { isEmpty } from "lodash";
+import '@fontsource/roboto';
+import { Typography } from '@material-ui/core';
+
 import "./Table.css";
 const Table = () => {
   const [bitData, setBitData] = useState({});
@@ -10,7 +12,7 @@ const Table = () => {
 
   let { bpi: prevBpi } = prevBitData.current;
   const getBitData = () => {
-    fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
+    fetch("https://api.coindesk.com/v1/bpi/currentprice.json")
       .then((data) => data.json())
       .then((data) => {
         setBitData((prevData) => {
@@ -22,37 +24,43 @@ const Table = () => {
   useEffect(() => {
     const interval = setInterval(getBitData, 6000);
     return () => clearInterval(interval);
-  },[]);
-  
+  }, []);
+
+  useEffect(()=>{
+    var tempArray=[...coinArray];
+    // tempArray.push(bitData);
+    tempArray= [bitData].concat(tempArray);
+    setCoinArray(tempArray)
+  },[bitData])
+
+  useEffect(()=>{
+    console.log(coinArray,'coinarray')
+  },[coinArray])
 
   function formatBitData(bpi, prevBpi) {
     let diff = 0;
-    if (isEmpty(bpi)) return <p>Loading</p>;
+    if (isEmpty(bpi)) return <p>Loading...</p>;
     let keys = Object.keys(bpi);
     let values = Object.values(bpi);
     let prevValuesExist = !isEmpty(prevBpi);
-    let array=[];
     let table = keys.map((i, index) => {
       if (prevValuesExist) {
         diff = (
           values[index].rate_float - Object.values(prevBpi)[index].rate_float
         ).toFixed(2);
       }
-      
 
       return (
         <React.Fragment>
-          <td key={index}>
-          {values[index].rate_float.toFixed(2)}
-          </td>
+          <td key={index}>{values[index].rate_float.toFixed(2)}</td>
           <td>
-          {prevValuesExist ? diff > 0
-              ? '⬆' + diff
-              : '⬇' + Math.abs(diff)
-            : 0.0}
-        </td>
+            {prevValuesExist
+              ? diff > 0
+                ? "⬆" + diff
+                : "⬇" + Math.abs(diff)
+              : 0.0}
+          </td>
         </React.Fragment>
-        
       );
     });
     return table;
@@ -60,10 +68,16 @@ const Table = () => {
 
   return (
     <div>
-      <h1 id="title">{bitData.chartName}</h1>
-      {/* <h3>Last updated Time :{bitData.time.updateduk}</h3> */}
-      <table className="table_header"  id="table">
-        <thead >
+      <Typography variant="h2" color="primary" align="center">
+        {bitData.chartName}
+      </Typography>
+      <Typography>
+      Last updated Time :{bitData && bitData.time && bitData.time.updateduk}
+      
+      </Typography>
+     
+      <table className="table_header" id="table">
+        <thead>
           <tr>
             <th>USD</th>
             <th>P/L</th>
@@ -74,9 +88,7 @@ const Table = () => {
           </tr>
         </thead>
 
-        <tbody className="row">
-          {formatBitData(bpi, prevBpi)}
-        </tbody>
+        <tbody className="row">{formatBitData(bpi, prevBpi)}</tbody>
       </table>
     </div>
   );
